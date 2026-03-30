0e28afc8074cd66b5e90bf7fb0aed885fae1c1eb
ValioEilax <119453927+ValioEilax@users.noreply.github.com>
2026-03-30 23:26:54 +0300

    1.12-1.14

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const anecdotes = [
+  "If it hurts, do it more often.",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  "The only way to go fast, is to go well.",
+];
+
+const App = () => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+
+  const maxVotes = Math.max(...votes);
+  const maxVotesIndex = votes.indexOf(maxVotes);
+
+  const handleSelected = () => {
+    let randomAnecdote;
+
+    do {
+      randomAnecdote = Math.floor(Math.random() * anecdotes.length);
+    } while (randomAnecdote === selected);
+
+    setSelected(randomAnecdote);
+  };
+
+  const handleVote = () => {
+    const votesCopy = [...votes];
+
+    votesCopy[selected] += 1;
+
+    setVotes(votesCopy);
+  };
+
+  return (
+    <>
+      <h2>Anecdote of the day</h2>
+      <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={handleSelected}>Next Anecdote</button>
+      <h2>Anecdote with most votes</h2>
+      <div>{anecdotes[maxVotesIndex]}</div>
+      <div>has {maxVotes} votes</div>
+    </>
+  );
+};
+
+export default App;
